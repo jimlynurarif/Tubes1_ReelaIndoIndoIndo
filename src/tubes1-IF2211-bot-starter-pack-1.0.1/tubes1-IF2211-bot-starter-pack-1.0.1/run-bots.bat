2891e1bdb188d38c4363def6f6b16e1bc6ecb35e
Rinnearu <rayhanridhar@gmail.com>
2024-03-09 23:37:43 +0700

    fix: bot runner fix

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
@@ -1,5 +1,5 @@
 @echo off
 @REM start cmd /c "python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo"
 @REM start cmd /c "python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo"
-start cmd /c "python main.py --logic Random --email=test2@email.com --name=JimGreedBot --password=123456 --team etimo"
-start cmd /c "python main.py --logic Tomerry --email=test3@email.com --name=Tomerry --password=724284 --team etimo"
+start cmd /c "python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo --board=2"
+start cmd /c "python main.py --logic Tomerry --email=test@email.com --name=stima --password=123456 --team etimo --board=2"
--- src/tubes1-IF2211-bot-starter-pack-1.0.1/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
@@ -2,5 +2,5 @@
 
 # python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo &
 # python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo &
-python main.py --logic Random --email=test2@email.com --name=JimGreedBot --password=123456 --team etimo
-python main.py --logic Tomerry --email=test3@email.com --name=Tomerry --password=724284 --team etimo
+python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo
+python main.py --logic Tomerry --email=test@email.com --name=stima --password=123456 --team etimo
